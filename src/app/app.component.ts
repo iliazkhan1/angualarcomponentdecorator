8a00573d361b3e2283f8349a70169d2dc8a89e92
iliazkhan1 <iliazkhan2003@gmail.com>
2025-03-21 11:19:24 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NewcomponentComponent } from './newcomponent/newcomponent.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,NewcomponentComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'componentdecorator';
+}
---
+++ src/app/newcomponent/newcomponent.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'iliaz-component',
+  imports: [],
+  templateUrl: './newcomponent.component.html',
+  styleUrl: './newcomponent.component.css'
+})
+export class NewcomponentComponent {
+
+}
